79d38e4ffe7e93ee321dbe460764c0c2d29b99fe
Mariasgreen <103245849+Mariasgreen@users.noreply.github.com>
2023-05-18 21:42:53 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import './modules/xhr.js';
+import './modules/acc.js';
+import './modules/burger.js';
+import './modules/fly.js';
+import './modules/tours.js';
+import timing from './modules/timer.js';
+const {deadline,
+  time} = timing;
+
+
+time(deadline);
+
+
+
+
+
+
